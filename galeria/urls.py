6a954c6e5cdb1cfd93868181428791748b96dfe0
Joel-Rodrigues <joelsandersonrodrigues@gmail.com>
2023-05-13 18:05:47 -0300

    commite do segundo curso django admin

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from galeria.views import index, imagem
+from galeria.views import index, imagem, buscar
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/', imagem, name='imagem'),
+    path('imagem/<int:foto_id>/', imagem, name='imagem'),
+    path('buscar/', buscar, name='buscar')
 ]
--- setup/settings.py
+++ setup/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria',
+    'galeria.apps.GaleriaConfig',
 ]
 
 MIDDLEWARE = [
@@ -126,6 +126,12 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# MEDIA
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
